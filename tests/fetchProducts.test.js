31d8c25528ceda26bdae27b59a61f60c10c4ac98
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-26 17:14:46 -0300

    requisito 8 - verificando se fetch chama a url correta

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,9 +8,15 @@ describe('1 - Teste a função fecthProducts', () => {
     return result = await fetchProducts('products');
   })
 
-  it('Teste se fetchProducts é uma função',() => {
+  it('Testa se fetchProducts é uma função',() => {
     expect(typeof fetchProducts).toBe('function');
   });
+
+  it('Testa se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', async () => {
+    const urlApi = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(urlApi);
+  })
 
   //fail('Teste vazio');
 });
