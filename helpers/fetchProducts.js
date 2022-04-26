cb09a5a2c713f2524fe0504554b086f1f2f77780
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-26 16:55:11 -0300

    requisito 8 testando se fecthProducts é uma funcao

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,7 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (product) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+  .then((result) => result.json())
+  .catch();
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,14 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+  beforeEach(async () => {
+    return result = await fetchProducts('products');
+  })
+
+  it('Teste se fetchProducts é uma função',() => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  //fail('Teste vazio');
 });
