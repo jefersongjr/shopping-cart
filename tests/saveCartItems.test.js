6a8dc171872a5eb61b41246f39b8c0132f6e73ac
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-29 22:50:22 -0300

    editando os testes saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -10,7 +10,7 @@ describe('4 - Teste a função saveCartItems', () => {
     expect(localStorage.setItem).toHaveBeenCalled();
   })
 
-  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', () => {
+  it('Teste se, ao executar saveCartItems com o argumento "<ol><li>Item</li></ol>" , o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
     const argument = '<ol><li>Item</li></ol>';
     saveCartItems(argument);
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', argument);
